ee39e5dd9114e586875d77d2d367b2851368c4c5
ViuRemix <152360526+ViuRemix@users.noreply.github.com>
2025-03-26 21:57:17 +0700

    Update default.nix

--- default.nix
+++ default.nix
@@ -7,4 +7,16 @@ pkgs.mkShell {
     pkgs.gcc
     pkgs.doc
   ];
+
+  # Example of a problematic package reference
+  # src = fetchurl {
+  #   url = "http://example.com/invalid-url.tar.gz";
+  #   sha256 = "0v1h1k1...";
+  # };
+
+  # Update the URL to a valid one
+  src = fetchurl {
+    url = "http://example.com/valid-url.tar.gz";
+    sha256 = "0v1h1k1...";
+  };
 }
