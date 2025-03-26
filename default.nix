58c86611a97d7c24e5c0cf0eb8c018e8727c70eb
ViuRemix/ <worksor.78@gmail.com>
2025-03-26 21:46:36 +0700

    fix the defaull...

--- default.nix
+++ default.nix
@@ -5,6 +5,6 @@ pkgs.mkShell {
     pkgs.python39
     pkgs.postgresql_16
     pkgs.gcc
-    pkgs.doc  # Replace 'dev' with 'doc'
+    pkgs.doc  # Ensure 'doc' is correct and replace 'dev' with 'doc'
   ];
 }
